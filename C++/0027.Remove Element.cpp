e37db434f34c081abaf5f99f35bc9f29f31e8ae4
user <haha001518@gmail.com>
2023-07-27 23:06:23 +0800

    2023/07/27

--- C++/0027.Remove Element.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        for(int i=size(nums)-1; i>=0; i--){
-            if(nums[i]==val) nums.erase(nums.begin() + i);
-        }
-        return size(nums);
-    }
-};
