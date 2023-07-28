2c7453ef38a57f2d82ef2434d49369e2186f73a9
user <haha001518@gmail.com>
2023-07-28 23:51:29 +0800

    2023/07/28*

---
+++ 0486.Predict the Winner.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        int sz = size(nums);
+        vector<int> dp(sz);
+        dp.assign(nums.begin(), nums.end()); // copy the value of nums to dp
+
+        // dp[j] -> from dp[0] to dp[j], the most scores player1 has over player2
+        for(int i=sz-1; i>=0; i--){
+            for(int j=i+1; j<sz; j++){
+                dp[j] = max(nums[i]-dp[j], nums[j]-dp[j-1]);
+            }
+        }
+        return dp[sz-1] >= 0;
+    }
+};
