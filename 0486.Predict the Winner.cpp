67d75de65ed937c1b42c19d14a3ed0dff9b7ada2
user <haha001518@gmail.com>
2023-07-29 00:00:55 +0800

    2023/07/28*

--- 0486.Predict the Winner.cpp
+++ 0486.Predict the Winner.cpp
@@ -8,6 +8,8 @@ public:
         // dp[j] -> from dp[0] to dp[j], the most scores player1 has over player2
         for(int i=sz-1; i>=0; i--){
             for(int j=i+1; j<sz; j++){
+                // nums[i]/[j] -> choose the nums[i] or nums[j]
+                // dp[j]/[j-1] -> scores won by the previous player
                 dp[j] = max(nums[i]-dp[j], nums[j]-dp[j-1]);
             }
         }
