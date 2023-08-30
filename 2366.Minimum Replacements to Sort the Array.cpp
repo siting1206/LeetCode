f8586e66224d5daa6ecb970d6218dc0d0a647ccc
siting1206 <haha001518@gmail.com>
2023-08-30 17:00:09 +0800

    2023/08/30*

--- 2366.Minimum Replacements to Sort the Array.cpp
+++ 2366.Minimum Replacements to Sort the Array.cpp
@@ -6,10 +6,8 @@ public:
         for(int i=n-2; i>=0; i--){
             if(nums[i]>last){
                 int tmp = nums[i]/last;
-                cout << nums[i] << " " << last << " " << endl;
                 if(nums[i]%last) tmp++;
                 last = nums[i]/tmp;
-                cout << tmp << " " << last << " " << endl;
                 ans += tmp-1;
             }
             else last = nums[i];
